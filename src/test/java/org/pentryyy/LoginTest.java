65a16501c9773b700c5bf430d9054d792a0b602e
pentryyy <m.v.volikov@gmail.com>
2025-04-04 11:18:22 +0300

    join all testcases in big one

--- src/test/java/org/pentryyy/LoginTest.java
+++ src/test/java/org/pentryyy/LoginTest.java
@@ -9,9 +9,7 @@ import org.pentryyy.components.ScreenshotUtils;
 import org.pentryyy.pages.LoginPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -49,41 +47,7 @@ public class LoginTest {
         }
     }
 
-    @Test
-    void testSuccessfulLogin() {
-        shouldTakeScreenshot = false;
-        loginPage.login("volikov_mikhail", "2104youtrack");
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-            .until(ExpectedConditions.urlContains("dashboard"));
-        assertTrue(driver.getCurrentUrl().contains("dashboard"));
-    }
-
-    @Test
-    void testFailureLogin() {
-        shouldTakeScreenshot = true;
-        loginPage.login("username", "password");
-        Assertions.assertTrue(loginPage.isLoginErrorMessageDisplayed(), 
-                      "Сообщение об ошибке не отображается");
-    }
-
-    @Test
-    public void testEmptyPasswordValidation() {
-        shouldTakeScreenshot = true;
-        loginPage.login("username", "");
-        Assertions.assertTrue(loginPage.isPasswordFieldHighlightedRed(), 
-                      "Поле пароля должно быть подсвечено красным");
-    }
-
-    @Test
-    public void testEmptyUsernameValidation() {
-        shouldTakeScreenshot = true;
-        loginPage.login("", "valid_password");
-        Assertions.assertTrue(loginPage.isUsernameFieldHighlightedRed(),
-                      "Поле логина должно быть подсвечено красным"
-        );
-    }
-
-    @ParameterizedTest(name = "[{index}] {0}")
+    @ParameterizedTest(name = "[{index}] {0}, {1}")
     @CsvFileSource(
         resources = "/LoginTestData.csv",
         numLinesToSkip = 1,
@@ -101,19 +65,25 @@ public class LoginTest {
         
         loginPage.login(safeUsername, safePassword);
         
-        // Пропуск теста, если логин или пароль пустые (Временно)
-        Assumptions.assumeTrue(
-            !safeUsername.isEmpty() && !safePassword.isEmpty(),
-            "Пропуск: пустой логин или пароль"
-        );
-
         if (expectedResult.equalsIgnoreCase("success")) {
+            shouldTakeScreenshot = false;
+
             new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.urlContains("dashboard"));
             assertTrue(driver.getCurrentUrl().contains("dashboard"));
         } else {
-            assertTrue(loginPage.isLoginErrorMessageDisplayed(), 
-                "Сообщение об ошибке не отображается");
+            shouldTakeScreenshot = true;
+
+            if (safePassword.isEmpty()) {
+                Assertions.assertTrue(loginPage.isPasswordFieldHighlightedRed(), 
+                              "Поле пароля должно быть подсвечено красным");
+            } else if (safeUsername.isEmpty()) {
+                Assertions.assertTrue(loginPage.isUsernameFieldHighlightedRed(),
+                              "Поле логина должно быть подсвечено красным");
+            } else {
+                assertTrue(loginPage.isLoginErrorMessageDisplayed(), 
+                   "Сообщение об ошибке не отображается");
+            }
         }
     }
 }
