b201deb1bdb913fa27219da5722688c0ea67e383
pentryyy <m.v.volikov@gmail.com>
2025-04-04 12:06:48 +0300

    LoginPage refactoring

--- src/test/java/org/pentryyy/pages/LoginPage.java
+++ src/test/java/org/pentryyy/pages/LoginPage.java
@@ -2,50 +2,53 @@ package org.pentryyy.pages;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 
-    private WebDriver     driver;
     private WebDriverWait wait;
 
-    private static final By USERNAME_FIELD      = By.id("username");
-    private static final By PASSWORD_FIELD      = By.id("password");
-    private static final By LOGIN_BUTTON        = By.cssSelector("[data-test='login-button']");
-    private static final By LOGIN_ERROR_MESSAGE = By.cssSelector(".header__text__error.ng-scope");
+    @FindBy(id = "username")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(css = "[data-test='login-button']")
+    private WebElement loginButton;
+
+    @FindBy(css = ".header__text__error.ng-scope")
+    private WebElement loginErrorMessage;
 
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
         this.wait   = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
-
-    private void waitForElement(By locator) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        PageFactory.initElements(driver, this);
     }
 
     public void login(String username, String password) {
-        waitForElement(USERNAME_FIELD);
-        driver.findElement(USERNAME_FIELD).sendKeys(username);
-        
-        waitForElement(PASSWORD_FIELD);
-        driver.findElement(PASSWORD_FIELD).sendKeys(password);
-        
-        waitForElement(LOGIN_BUTTON);
-        driver.findElement(LOGIN_BUTTON).click();
+        wait.until(ExpectedConditions.visibilityOf(usernameField)).sendKeys(username);
+        wait.until(ExpectedConditions.visibilityOf(passwordField)).sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
     }
 
     public boolean isLoginErrorMessageDisplayed() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(LOGIN_ERROR_MESSAGE))
-                   .isDisplayed();
+        try {
+            wait.until(ExpectedConditions.visibilityOf(loginErrorMessage));
+            return loginErrorMessage.isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
     }
 
     public boolean isPasswordFieldHighlightedRed() {
         try {
-            wait.until(ExpectedConditions.attributeContains(PASSWORD_FIELD, "class", "ring-input_error"));
+            wait.until(ExpectedConditions.attributeContains(passwordField, "class", "ring-input_error"));
             return true;
         } catch (TimeoutException e) {
             return false;
@@ -54,7 +57,7 @@ public class LoginPage {
 
     public boolean isUsernameFieldHighlightedRed() {
         try {
-            wait.until(ExpectedConditions.attributeContains(USERNAME_FIELD, "class", "ring-input_error"));
+            wait.until(ExpectedConditions.attributeContains(usernameField, "class", "ring-input_error"));
             return true;
         } catch (TimeoutException e) {
             return false;
