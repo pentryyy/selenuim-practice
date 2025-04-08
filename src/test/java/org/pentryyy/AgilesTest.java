9bc4b904bc5076119dbcae8262b53b34a3cb173e
pentryyy <m.v.volikov@gmail.com>
2025-04-08 10:52:40 +0300

    Test comit

---
+++ src/test/java/org/pentryyy/AgilesTest.java
@@ -0,0 +1,56 @@
+package org.pentryyy;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.pentryyy.components.custom.driver.CustomChromeWebDriverManager;
+import org.pentryyy.components.ScreenshotUtils;
+import org.pentryyy.pages.AgilesPage;
+import org.pentryyy.pages.IssuesPage;
+import org.pentryyy.pages.LoginPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.openqa.selenium.WebDriver;
+
+public class AgilesTest {
+
+    private WebDriver  driver;
+    private LoginPage  loginPage;
+    private AgilesPage agilesPage;
+
+    private AtomicBoolean shouldTakeScreenshot = new AtomicBoolean();
+
+    @BeforeEach
+    void setUp() {
+        driver = CustomChromeWebDriverManager.getDriver("http://193.233.193.42:9091/agiles");
+        
+        agilesPage = new AgilesPage(driver);
+        loginPage  = new LoginPage(driver);
+
+        loginPage.login("volikov_mikhail", "2104youtrack");
+    }
+
+    @AfterEach
+    void tearDown(TestInfo testInfo) {
+        if (shouldTakeScreenshot.get() == true) {
+            ScreenshotUtils.takeScreenshot(driver, testInfo);
+        }
+        CustomChromeWebDriverManager.quitDriver();
+    }
+    
+    @Test
+    void testAddNewCard() {
+        try {
+
+            Assertions.assertTrue(agilesPage.isNewCardAdded(),
+                          "Новая карточка не добавлена");
+            
+            shouldTakeScreenshot.set(false);
+        } catch (Throwable e) {
+            shouldTakeScreenshot.set(true);
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/org/pentryyy/pages/AgilesPage.java
@@ -0,0 +1,89 @@
+package org.pentryyy.pages;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AgilesPage {
+
+    private WebDriver     driver;
+    private WebDriverWait wait;
+
+    // ------------------------ Данные для теста по добавлению карточки -----------------------
+
+    public AgilesPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait   = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isNewCardAdded() {
+        try {
+
+            // String timestamp = LocalDateTime
+            //     .now()
+            //     .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        
+            // String heading     = "Тестовый заголовок " + timestamp; 
+            // String description = "Тестовое описание "  + timestamp;
+
+            Thread.sleep(10000);
+
+            WebElement dropdown = driver.findElement(By.className("yt-agile-board__create-button_new button_f3d2 button_f3d2 heightS_c114 primary_e13e buttonWithoutIcon_b3e8"));
+            Select select = new Select(dropdown);
+            select.selectByVisibleText("Свимлэйн...");
+
+
+            // yt-agile-board__create-button_new button_f3d2 button_f3d2 heightS_c114 primary_e13e buttonWithoutIcon_b3e8
+            // yt-agile-board__create-button_new button_f3d2 button_f3d2 heightS_c114 primary_e13e buttonWithoutIcon_b3e8
+            // yt-agile-board__create-button_new button_f3d2 button_f3d2 heightS_c114 primary_e13e buttonWithoutIcon_b3e8
+            // yt-agile-board__create-button_new button_f3d2 button_f3d2 heightS_c114 primary_e13e buttonWithoutIcon_b3e8
+
+            // wait.until(ExpectedConditions.elementToBeClickable(swimlaneButton)).click();
+
+            // By buttonLocator = By.xpath("//button[contains(@class, 'action_f9dd') and @data-test='ring-list-item-label']");
+            // wait.until(ExpectedConditions.elementToBeClickable(buttonLocator));
+
+            // WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(buttonLocator));
+            // button = wait.until(d -> {
+            //     driver.navigate().refresh();  // если требуется обновление
+            //     return driver.findElement(buttonLocator);
+            // });
+
+            // WebElement swimlane = wait.until(ExpectedConditions.presenceOfElementLocated(
+            //     By.xpath("//span[@class='label_f8a6' and contains(text(),'Свимлэйн')]")
+            // ));
+
+
+            // By buttonSelector = By.cssSelector("button[data-test='ring-list-item-label']");
+            // WebElement button = driver.findElement(buttonSelector);
+            // button.click();
+
+            // System.out.println(swimlaneButton.getText()+"\n");
+
+            // swimlaneButton.click();
+
+            // wait.until(ExpectedConditions.elementToBeClickable(textarea)).sendKeys(heading);
+            // wait.until(ExpectedConditions.elementToBeClickable(editor)).sendKeys(description);
+            
+            return true;
+        } catch (TimeoutException | InterruptedException e) {
+            return false;
+        }
+    }
+}
